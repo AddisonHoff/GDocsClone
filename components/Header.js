70055fb086f89b8c30891574fdf3bb6e2f328dcf
Addison <addisonghoff@gmail.com>
2022-03-16 09:42:59 -0600

    firstCommit

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Button from '@material-tailwind/react/Button'
+import Icon from '@material-tailwind/react/Icon'
+function Header() {
+    return (
+        <div className="flex items-center">
+            Header
+        </div>
+
+    )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import "@material-tailwind/react/tailwind.css";
 
+// Like a global starting point or the app, like App.js in React
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
