b0e4b14eca5a6ed3f5135727a42abbebe96a972d
Addison <addisonghoff@gmail.com>
2022-03-16 16:19:45 -0600

    Revert "add docs page, begin auth"
    This reverts commit 50b43ea12c08725099df03e8d3a94fa2103d9e8e.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import "@material-tailwind/react/tailwind.css";
-import 'material-icons/iconfont/material-icons.css';
-import { SessionProvider } from "next-auth/react"
 
 // Like a global starting point or the app, like App.js in React
-function MyApp({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
-  return(
-    <div>
-      <Head>
-
-      </Head>
-      <SessionProvider session={session}>
-    <Component {...pageProps} />  
-    </SessionProvider>
-    </div>
-
-  ) 
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
 
 export default MyApp
