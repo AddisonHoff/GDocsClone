78ec2b903b5e53e31ac3ec78463ee5d5d39412cc
Addison <addisonghoff@gmail.com>
2022-03-17 19:31:18 -0600

    finishedT

--- pages/doc/[id].js
+++ pages/doc/[id].js
@@ -1,16 +1,17 @@
-import { useSession } from 'next-auth/client'
+import { useSession, getSession } from 'next-auth/client'
 import Login from '../../components/Login'
 import { useRouter } from 'next/dist/client/router'
 import { useDocumentOnce } from 'react-firebase-hooks/firestore'
 import { db } from '../../firebase'
-
+import Button from '@material-tailwind/react/Button'
+import TextEditor from '../../components/TextEditor'
 
 
 
 export default function Doc() {
 
     const [session] = useSession()
-    if (!session) return <Login > < /Login>
+    if (!session) return <Login></Login>
 
     const router = useRouter()
     const { id } = router.query
@@ -21,35 +22,42 @@ export default function Doc() {
 
 
     // Redirect user if they try to access URL they don't have access to
-    if (!loadingSnapshot && !snapshot ? .data() ? .fileName) {
+    if (!loadingSnapshot && !snapshot?.data()?.fileName) {
         router.replace("/")
     }
 
-    return ( <
-        div >
-        <
-        header className = 'flex justify-between items-center p-3 pb-1' >
-        <
-        button onClick = {
-            () => Router.push('/') }
-        class = "rounded-full focus:outline-none h-20 w-20 flex items-center justify-center" >
-        <
-        img src = '/logo.png'
-        className = "pokeball"
-        alt = "logo" / >
-        <
-        /button> <
-        div className = "flex-grow px-2" >
-        <
-        h2 class >
-        { snapshot ? .data() ? .fileName } < /h2> <
-        /div>
+    return (
+        <div>
+        <div>
+
+        <header className="sticky top-0 z-50 flex items-center px-4 shadow-md bg-white">
+            <div           onClick={() => router.push('/')}
+                    className="ml-2 py-10 rounded-full focus:outline-none h-10 w-10 flex items-center justify-center hover:cursor-pointer">
+         
+                    <img src='/logo.png'
+                        className="logo" alt="logo" />
+                </div>
+            {/* <Icon name="description" size="5xl" color="blue"></Icon> */}
+            <h1 className='ml-2 text-black text-4xl font-semibold pr-5'>Divination</h1>
+
+        </header>
+
+        <TextEditor/>
 
 
-        <
-        /div> <
-        /header > <
-        h1 > TEST < /h1> <
-        /div >
+        </div>
+
+        </div >
     )
+}
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context)
+
+        return {
+            props: {
+            session
+      }
+    }
+    
 }
