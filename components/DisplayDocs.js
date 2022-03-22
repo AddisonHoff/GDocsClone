f0d6db1dc2cfd7a167f2033c0b76092c2ca3e0ee
Addison <addisonghoff@gmail.com>
2022-03-21 19:06:16 -0600

    css

--- components/DisplayDocs.js
+++ components/DisplayDocs.js
@@ -24,8 +24,8 @@ export default function DisplayDocs() {
 
 
     return (
-        <div className='h-screen bg-slate-900'>
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
+        <div className='h-screen bg-slate-900 justify-center'>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-7xl mx-auto">
 
         {snapshot?.docs.map(doc => (
                         <DocumentRow
