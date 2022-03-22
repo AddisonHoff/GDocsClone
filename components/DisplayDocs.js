8e8291802e36880b806c026c712303fbb4bdad2d
Addison <addisonghoff@gmail.com>
2022-03-21 18:52:03 -0600

    WIP HOMEPAGE CSS

--- components/DisplayDocs.js
+++ components/DisplayDocs.js
@@ -24,25 +24,22 @@ export default function DisplayDocs() {
 
 
     return (
+        <div className='h-screen bg-slate-900'>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
 
-            <table class="table-auto border-collapse w-full">
-            <thead class="bg-black">
-                <tr class="rounded-lg font-medium text-white text-center text-xl">
-                <th class="px-4 py-2">Title</th>
-                <th class="px-4 py-2">Views</th>
-                </tr>
-            </thead>
-            <tbody class="text-sm font-normal text-gray-700 text-center">
-            {snapshot?.docs.map(doc => (
-                <DocumentRow
-                key={doc.id}
-                id={doc.id}
-                fileName={doc.data().fileName}
-                date={doc.data().timestamp}
-                >
-                </DocumentRow>
-            ))}
-            </tbody>
-            </table>
+        {snapshot?.docs.map(doc => (
+                        <DocumentRow
+                        key={doc.id}
+                        id={doc.id}
+                        fileName={doc.data().fileName}
+                        date={doc.data().timestamp}
+                        >
+                        </DocumentRow>
+                    ))}
+
+
+
+        </div>
+        </div>
     )
     }
--- components/DocumentRow.js
+++ components/DocumentRow.js
@@ -6,12 +6,16 @@ export default function DocumentRow({id, fileName, date}) {
 
     const Router = useRouter();
     return(
-      <tr 
+
+      
+      <div 
       onClick={() => Router.push(`/doc/${id}`)}
-      class="hover:bg-gray-100 border-gray-200">
-        <td class="px-4 py-4">{fileName}</td>
-        <td class="px-4 py-4">{date?.toDate().toLocaleDateString()}</td>
-      </tr>
+      class="card m-2 cursor-pointer border border-white rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+      <div class="m-3">
+        <h2 class="text-lg mb-2 text-white">{fileName}
+       <span class="text-sm text-black font-mono bg-white inline rounded-full px-2 align-top float-right animate-pulse">{date?.toDate().toLocaleDateString()}</span></h2>
+      </div>
+    </div>
     )
 }
 
